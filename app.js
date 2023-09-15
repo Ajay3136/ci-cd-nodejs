a34bd8ef888de21504959a4a4fd8e8be745b9bd5
AJAY KUMAR <66915588+Ajay3136@users.noreply.github.com>
2023-09-15 13:55:25 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello, World!\n');
+  const timeoutInMilliseconds = 15000; // 15 seconds
+  setTimeout(() => {
+    // Code to execute after the timeout (e.g., stop the server)
+    console.log('Timeout reached. Stopping the server.');
+    server.close(() => {
+      console.log('Server has been stopped.');
+    });
+  }, timeoutInMilliseconds);
 });
 
 const port = process.env.PORT || 3000;
