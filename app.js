e77615238a0009e012eb94ea4fa86257d0deaa6a
Ajay Johar <ajayjohar3136@gmail.com>
2023-09-15 14:16:25 -0400

    update app.js

--- app.js
+++ app.js
@@ -3,17 +3,17 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello, World!\n');
-  const timeoutInMilliseconds = 15000; // 15 seconds
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}/`);
+  
+  // Set a timeout to stop the server after 10 seconds (10000 milliseconds)
   setTimeout(() => {
-    // Code to execute after the timeout (e.g., stop the server)
-    console.log('Timeout reached. Stopping the server.');
+    console.log('Stopping the server...');
     server.close(() => {
       console.log('Server has been stopped.');
     });
-  }, timeoutInMilliseconds);
-});
-
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  }, 10000);
 });
